92804b75ad89e934cc36db106b5fe8289d337725
Sandesh Nirmal <sanneshnirmalme@gmail.com>
2025-04-09 14:50:22 +0000

    changed on top

--- portfolio/landing_page/static/javascript/script.js
+++ portfolio/landing_page/static/javascript/script.js
@@ -19,4 +19,12 @@ window.addEventListener("scroll", () => {
   console.log("works");
 });
 
+const parallaxSections = document.querySelectorAll(".parallax-section");
 
+window.addEventListener("scroll", () => {
+  parallaxSections.forEach((section) => {
+    const speed = section.dataset.speed || 0.5; // Adjust the speed (lower value = slower movement)
+    const offset = window.scrollY * speed;
+    section.style.backgroundPositionY = `${offset}px`;
+  });
+});
